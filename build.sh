8ddc7a312aa2cf8d4e44c473b6612c50c77195d8
Sander Huijsen <sander.huijsen@gmail.com>
2018-09-17 22:32:28 +0800

    Don't download if we don't have to. Don't unpack if we don't have to.

--- build.sh
+++ build.sh
@@ -3,15 +3,17 @@
 USERNAME=sandyman
 IMAGENAME=adaptlearning
 
-AAT_VER=0.5.0
+TARGET=adapt_authoring
+VERSION=0.5.0
 
-tarball=v${AAT_VER}.tar.gz
+tarball=v${VERSION}.tar.gz
 
-if [ ! -f ${tarball} ]; then
-    curl -L https://github.com/adaptlearning/adapt_authoring/archive/${tarball} -o ${tarball}
+if [ ! -d ${TARGET} ]; then
+    if [ ! -f ${tarball} ]; then
+        curl -L https://github.com/adaptlearning/adapt_authoring/archive/${tarball} -o ${tarball}
+    fi
+    tar -xzf ${tarball}
+    mv adapt_authoring-${VERSION} ${TARGET}
 fi
 
-tar -xzf ${tarball}
-mv adapt_authoring-${AAT_VER} adapt_authoring
-
-docker build --rm --build-arg aat_ver=${AAT_VER} -t ${USERNAME}/${IMAGENAME} .
+docker build --rm -t ${USERNAME}/${IMAGENAME} .
